1371b4a360779a7c628da3586aad51d1376d0348
SalmaDarrouji <Salmadarrouji94@gmail.com>
2023-09-10 19:07:46 +0100

    Make a reservation : All Steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
 import { CopyNavbarComponent } from './navbar copy/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { EquipmentCardComponent } from './equipment-card/equipment-card.component';
@@ -13,6 +16,10 @@ import { ProjectCardComponent } from './project-card/project-card.component';
 import { NewReservationPageComponent } from './new-reservation-page/new-reservation-page.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { AdminnavbarComponent } from './adminnavbar/adminnavbar.component';
+import { MatStepperModule } from '@angular/material/stepper';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchbarComponent } from './searchbar/searchbar.component'; // Import MatStepperModule
+
 
 @NgModule({
   declarations: [
@@ -26,11 +33,16 @@ import { AdminnavbarComponent } from './adminnavbar/adminnavbar.component';
     NewReservationPageComponent,
     SidebarComponent,
     AdminnavbarComponent,
+    SearchbarComponent,
   ],
   imports: [
     BrowserModule,
+    MatPaginatorModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     HttpClientModule,
+    MatStepperModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
